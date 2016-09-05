23d75f1fdd1b7c9b4c4c917ddbf241edcf46b976
zgsunshine1 <zgsunshine1@163.com>
2016-09-05 19:32:21 +0800

    change export function

--- src/ti_server.erl
+++ src/ti_server.erl
@@ -4,13 +4,11 @@
 
 -module(ti_server).
 -behaviour(gen_server).
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3,start_link/1]).
 
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([start_link/1]).
-
 
 
 %% ====================================================================
